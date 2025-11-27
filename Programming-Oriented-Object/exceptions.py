c57146e9e91ba529b335aafd9a36398c123bdf7a
salomon-tech <salomondev7@gmail.com>
2025-11-28 00:13:52 +0200

    exceptions en python

--- Programming-Oriented-Object/exceptions.py
+++ Programming-Oriented-Object/exceptions.py
@@ -2,10 +2,11 @@
 class SoldinsiffisantError(Exception):
   pass
 
-class Wallet:
+class PorteMoney:
   def __init__(self, titulaire, solde_initial=0):
     self.titulaire = titulaire
     self.solde = solde_initial
+
 
   def deposer(self, montant):
     if montant <= 0:
@@ -13,14 +14,17 @@ class Wallet:
     self.solde += montant
     return f"depot de {montant}$ effectuer"
   
+
   def retirer(self, montant):
     if montant <= 0:
       raise ValueError("entrer un montant valide")
     if montant > self.solde:
       raise SoldinsiffisantError("solde insuffisant")
     return f"retrer de {montant}$ effectuer avec succee"
-  
-compte = Wallet("salomon", 50000)
+
+
+if __name__ == '__main__':
+  compte = PorteMoney("salomon", 50000)
 try:
   print(compte.deposer(500))
   print(compte.retirer(10000))
@@ -28,4 +32,3 @@ except SoldinsiffisantError as e:
   print(f"erreur: {e}")
 except ValueError as e:
   print(f"erreur de valeur : {e}")
-  print(f"nouveau solde : {Wallet}")
