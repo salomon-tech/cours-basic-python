e1ab4c899727fe5f418ac895cfaf59b1df6cad14
salomon-tech <salomondev7@gmail.com>
2025-11-27 06:57:15 +0200

    fix some error in class-object.py

--- Programming-Oriented-Object/class-object.py
+++ Programming-Oriented-Object/class-object.py
@@ -1,19 +1,22 @@
-class Peaple:
+class Person:
 
-  #class atribute
-  race = "nigga"
+  # class attribute
+  race = "human"
 
   def __init__(self, name, age):
     self.name = name
     self.age = age
 
   def introduction(self):
-    return f"i'm {self.name} and i've {self.age} years old"
+    return f"I'm {self.name} and I'm {self.age} years old"
   
   def birth_day(self):
-    self.birth_day += 1
-    return f"happy birthday to me! now i've got {self.age} years old"
-  
-  person_info = Peaple("salomon", 17)
+    # increment the person's age
+    self.age += 1
+    return f"Happy birthday to me! Now I've got {self.age} years old"
+
+
+if __name__ == "__main__":
+  person_info = Person("salomon", 17)
   print(person_info.introduction())
   print(person_info.birth_day())
