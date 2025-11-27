bdec9ffaf7ca9308921a00282c4999ebf1178fee
salomon-tech <salomondev7@gmail.com>
2025-11-28 00:28:52 +0200

    les conditions avec python

--- condition.py
+++ condition.py
@@ -1,3 +1,7 @@
+"""Les conditions en python
+
+sont des structures de controle qui permettent d'executer des blocs de code selon des conditions specifiques."""
+
 age = 18
 if age < 18:
   print("child")
