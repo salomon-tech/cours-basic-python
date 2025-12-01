f3d6f10663141e447c2032a7fd22aa33327fb892
salomon-tech <salomondev7@gmail.com>
2025-12-01 14:16:54 +0200

    into a l'interface graphique avec tkinter python

--- i-graphique/interface_graphique.py
+++ i-graphique/interface_graphique.py
@@ -49,5 +49,31 @@ class Interface:
     ttk.Label(input_frame, text='nom : ').grid(row=0, column=0, sticky=tk.w, padx=5, pady=5)
     self.entry_nom = ttk.Entry(input_frame, width=30)
     self.entry_nom.grid(row=0, column=1, padx=5, pady=5)
+
+    ttk.Label(input_frame, text="email : ").grid(row=1, column=0, sticky=tk.w, padx=5, pady=5)
+    self.entry_email = ttk.Entry(input_frame, width=30)
+    self.entry_email.grid(row=1, colomn=1, padx=5, pady=5)
+
+    # les boutons
+
+    button = ttk.Frame(main_frame)
+    button.pack(pady=20)
+
+    ttk.Button(button, text="ajouter", command=self.ajouter).pack(side=tk.LEFT, padx=5)
+    ttk.Button(button, text="afficher", command=self.afficher).pack(side=tk.LEFT, padx=5)
+    ttk.Button(button, text="quiter", command=self.afficher).pack(side=tk.LEFT, padx=5)
+
+    # treeview pour l'affichage des donnees
+
+    columns = ("id", "nom", "email")
+    self.tree = ttk.Treeview(main_frame, columns=columns, show="headings", height=15)
+
+    # definition des headings
+
+    for colonne in columns:
+      self.tree.heading(colonne, text=colonne)
+      self.tree.column(colonne, width=150)
+
+    self.tree.pack(fill=tk.BOTH, exepend=True, pady=10)
     
 
