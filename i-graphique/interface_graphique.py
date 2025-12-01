b735bcc5b07c39548d22230f9373dacd118afc57
salomon-tech <salomondev7@gmail.com>
2025-12-01 12:30:10 +0200

    into a l'interface graphique avec tkinter python

--- i-graphique/interface_graphique.py
+++ i-graphique/interface_graphique.py
@@ -15,3 +15,28 @@ class Interface:
     self.style.configure("TFrame", background="#f0f0f0")
     self.style.configure("TLabel", background="#f0f0f0")
     self.style.configure("TButton", background="#4caf50", foreground="#ffffff", font=("arial", 10, "bold"))
+    self.style.configure("Header.TLabel", font=("arial", 14, "bold"), foreground=("#333333"))
+
+
+    #  intro a la creation des widgets avec tkinter en python
+
+    self.creer_widgets()
+
+  def creer_widgets(self):
+
+    # le frame principal
+    main_frame = ttk.Frame(self.root, padding="20")
+    main_frame.pack(fill="both", expend=True)
+
+    # titre
+
+    titre = ttk.Label(main_frame)
+    titre.pack(pady=10)
+
+    # les frames pour les entree
+
+    input_frame = ttk.Frame(main_frame, padding="10")
+    input_frame.pack(pady=10)
+
+    # les boutons
+
